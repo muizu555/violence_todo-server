937464686800c0f11360b88b915df4d0ba0fedfb
muizu <muizu0920555@protonmail.com>
2024-02-19 21:52:21 +0900

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"violence_todo/infra"
+	"violence_todo/usecase"
 )
 
 func main() {
 	db := infra.NewDB()
+	todoRepository := infra.NewTodo(db)
+	todoUsecase := usecase.NewTodo(todoRepository)
+	todoHandler := hander
+
 }
